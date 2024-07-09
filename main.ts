4862ebaf65785d8a5bc3766a9a87c2d2b0846cdd
Le Thanh <thanhlth99@gmail.com>
2024-07-09 23:32:56 +0700

    answer

--- main.ts
+++ main.ts
@@ -1,5 +1,17 @@
-import { GetRecipes } from "./supporting-files/data-access";
+import {
+  GetRecipes,
+  GetProductsForIngredient,
+  GetBaseUoM,
+} from "./supporting-files/data-access";
+import {
+  GetCostPerBaseUnit,
+  ConvertUnits,
+  GetNutrientFactInBaseUnits,
+  aggregateNutrients,
+  sortKeysAlphabetically,
+} from "./supporting-files/helpers";
 import { ExpectedRecipeSummary, RunTest } from "./supporting-files/testing";
+import { NutrientFact } from "./supporting-files/models";
 
 console.clear();
 console.log("Expected Result Is:", ExpectedRecipeSummary);
@@ -11,6 +23,62 @@ const recipeSummary: any = {}; // the final result to pass into the test functio
  * YOUR CODE GOES BELOW THIS, DO NOT MODIFY ABOVE
  * (You can add more imports if needed)
  * */
+const MAX_VALUE = Number.MAX_SAFE_INTEGER;
+
+recipeData.forEach((recipeItem) => {
+  // initialize cost of the recipe
+  let costOfRecipe = 0;
+  // initialize the nutrients of the recipe
+  let nutrientReceipt: NutrientFact[] = [];
+  recipeItem.lineItems.forEach((lineItem) => {
+    // get each ingredient
+    let costOfIngredient = MAX_VALUE;
+    // initialize the nutrient of the ingredient
+    let nutrientItem: NutrientFact[] = [];
+
+    // get the products of the ingredient
+    const productsForIngredient = GetProductsForIngredient(lineItem.ingredient);
+    // get the base unit of measure of the ingredient
+    const baseUoM = GetBaseUoM(lineItem.unitOfMeasure.uomType);
+    // convert the unit of measure of the ingredient to the base unit of measure of the ingredient
+    const convertUnitBasedCost = ConvertUnits(
+      lineItem.unitOfMeasure,
+      baseUoM.uomName,
+      baseUoM.uomType
+    );
+
+    // find the product with the lowest cost per base unit of measure
+    productsForIngredient.forEach((product) => {
+      product.supplierProducts.forEach((supplier) => {
+        // get the cost per base unit of measure of the supplier product
+        const costPerBaseUnit = GetCostPerBaseUnit(supplier);
+        // update the cost of ingredient and the nutrientItem if the cost per base unit of measure is lower than the current cost of ingredient
+        if (costPerBaseUnit < costOfIngredient) {
+          costOfIngredient = costPerBaseUnit;
+          nutrientItem = product.nutrientFacts;
+        }
+      });
+    });
+
+    // update the cost of the recipe 
+    costOfRecipe += costOfIngredient * convertUnitBasedCost.uomAmount;
+    // get the nutrients of the product in base unit and add them to the nutrient receipt of the recipe
+    const nutrient = nutrientItem.map((nutrient) =>
+      GetNutrientFactInBaseUnits(nutrient)
+    );
+    nutrientReceipt = nutrientReceipt.concat(nutrient);
+  });
+
+  // calculate the sum of nutrient and sort alphabetically
+  const nutrientsAtCheapestCost = sortKeysAlphabetically(
+    aggregateNutrients(nutrientReceipt)
+  );
+
+  recipeSummary[recipeItem.recipeName] = {
+    cheapestCost: costOfRecipe,
+    nutrientsAtCheapestCost: nutrientsAtCheapestCost,
+  };
+});
 
 /*
  * YOUR CODE ABOVE THIS, DO NOT MODIFY BELOW
--- supporting-files/helpers.ts
+++ supporting-files/helpers.ts
@@ -3,88 +3,132 @@ import {
     SupplierProduct,
     UnitOfMeasure,
     UoMName,
-    UoMType
-} from "./models";
-import {GetBaseUoM, GetUnitsData, NutrientBaseUoM} from "./data-access";
-
-/*
-* Converts from one unit of measure to another requested unit of measure
-*/
-export function ConvertUnits(
+    UoMType,
+  } from "./models";
+  import { GetBaseUoM, GetUnitsData, NutrientBaseUoM } from "./data-access";
+  
+  /*
+   * Converts from one unit of measure to another requested unit of measure
+   */
+  export function ConvertUnits(
     fromUoM: UnitOfMeasure,
     toUoMName: UoMName,
     toUoMType: UoMType
-): UnitOfMeasure {
+  ): UnitOfMeasure {
     if (fromUoM.uomName === toUoMName && fromUoM.uomType === toUoMType)
-        return fromUoM;
-
+      return fromUoM;
+  
     const conversionRate = GetUnitsData().find(
-        (x) =>
-            x.fromUnitName === fromUoM.uomName &&
-            x.fromUnitType === fromUoM.uomType &&
-            x.toUnitName === toUoMName &&
-            x.toUnitType === toUoMType
+      (x) =>
+        x.fromUnitName === fromUoM.uomName &&
+        x.fromUnitType === fromUoM.uomType &&
+        x.toUnitName === toUoMName &&
+        x.toUnitType === toUoMType
     );
-
+  
     if (!conversionRate)
-        throw new Error(`Couldn't convert ${fromUoM.uomName} to ${toUoMName}`);
-
+      throw new Error(`Couldn't convert ${fromUoM.uomName} to ${toUoMName}`);
+  
     return {
-        uomAmount: fromUoM.uomAmount * conversionRate.conversionFactor,
-        uomName: toUoMName,
-        uomType: toUoMType
+      uomAmount: fromUoM.uomAmount * conversionRate.conversionFactor,
+      uomName: toUoMName,
+      uomType: toUoMType,
     };
-}
-
-/*
-* Takes in two units of measure and returns the sum of both in the same base unit as A
-*/
-export function SumUnitsOfMeasure(
+  }
+  
+  /*
+   * Takes in two units of measure and returns the sum of both in the same base unit as A
+   */
+  export function SumUnitsOfMeasure(
     uomA: UnitOfMeasure,
     uomB: UnitOfMeasure
-): UnitOfMeasure {
+  ): UnitOfMeasure {
     const convertedUomB = ConvertUnits(uomB, uomA.uomName, uomA.uomType);
     return {
-        uomAmount: uomA.uomAmount + convertedUomB.uomAmount,
-        uomName: uomA.uomName,
-        uomType: uomA.uomType
+      uomAmount: uomA.uomAmount + convertedUomB.uomAmount,
+      uomName: uomA.uomName,
+      uomType: uomA.uomType,
     };
-}
-
-/*
-* Calculates the cost price for a given supplier product,
-* in the base unit of measure for that product
-*/
-export function GetCostPerBaseUnit(supplierProduct: SupplierProduct): number {
+  }
+  
+  /*
+   * Calculates the cost price for a given supplier product,
+   * in the base unit of measure for that product
+   */
+  export function GetCostPerBaseUnit(supplierProduct: SupplierProduct): number {
     const baseUnitOfMeasure = GetBaseUoM(
-        supplierProduct.supplierProductUoM.uomType
+      supplierProduct.supplierProductUoM.uomType
     );
-
+  
     const converted = ConvertUnits(
-        supplierProduct.supplierProductUoM,
-        baseUnitOfMeasure.uomName,
-        baseUnitOfMeasure.uomType
+      supplierProduct.supplierProductUoM,
+      baseUnitOfMeasure.uomName,
+      baseUnitOfMeasure.uomType
     );
-
+  
     return supplierProduct.supplierPrice / converted.uomAmount;
-}
-
-/*
-* Takes in a nutrient fact and converts its measurements into the appropriate
-* base unit of measure for nutrients
-*/
-export function GetNutrientFactInBaseUnits(fact: NutrientFact): NutrientFact {
+  }
+  
+  /*
+   * Takes in a nutrient fact and converts its measurements into the appropriate
+   * base unit of measure for nutrients
+   */
+  export function GetNutrientFactInBaseUnits(fact: NutrientFact): NutrientFact {
     return {
-        nutrientName: fact.nutrientName,
-        quantityAmount: ConvertUnits(
-            fact.quantityAmount,
-            NutrientBaseUoM.uomName,
-            NutrientBaseUoM.uomType
-        ),
-        quantityPer: ConvertUnits(
-            fact.quantityPer,
-            NutrientBaseUoM.uomName,
-            NutrientBaseUoM.uomType
-        )
+      nutrientName: fact.nutrientName,
+      quantityAmount: ConvertUnits(
+        fact.quantityAmount,
+        NutrientBaseUoM.uomName,
+        NutrientBaseUoM.uomType
+      ),
+      quantityPer: ConvertUnits(
+        fact.quantityPer,
+        NutrientBaseUoM.uomName,
+        NutrientBaseUoM.uomType
+      ),
     };
-}
+  }
+  
+  export const aggregateNutrients = (
+    nutrients: NutrientFact[]
+  ): { [key: string]: NutrientFact } => {
+    const resultNutrientFact: { [key: string]: NutrientFact } = {};
+  
+    nutrients.forEach((item) => {
+      const { nutrientName, quantityAmount, quantityPer } = item;
+  
+      // If the nutrient doesn't exist in the result object, create a new entry
+      if (!resultNutrientFact[nutrientName]) {
+        resultNutrientFact[nutrientName] = {
+          nutrientName,
+          quantityAmount: { ...quantityAmount },
+          quantityPer: quantityPer,
+        };
+      } else {
+    // If the nutrient does exist, add the quantity amounts together
+        resultNutrientFact[nutrientName].quantityAmount.uomAmount +=
+          quantityAmount.uomAmount;
+      }
+    });
+  
+    return resultNutrientFact;
+  };
+  
+  // Function to sort keys in an object alphabetically, ignoring case sensitivity
+  const compareFunction = (a: string, b: string): number =>
+    a.localeCompare(b, "en", { sensitivity: "base" });
+  
+  // Function to sort keys in an object alphabetically
+  export const sortKeysAlphabetically = (obj: {
+    [key: string]: NutrientFact;
+  }): { [key: string]: NutrientFact } => {
+    const sortedKeys = Object.keys(obj).sort(compareFunction);
+    const sortedObj: { [key: string]: NutrientFact } = {};
+  
+    sortedKeys.forEach((key) => {
+      sortedObj[key] = obj[key];
+    });
+  
+    return sortedObj;
+  };
+  
