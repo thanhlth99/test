4a0c218746ef9b659298ae95f60771e88837b41f
Le Thanh <thanhlth99@gmail.com>
2024-07-09 22:53:03 +0700

    initial test

---
+++ main.ts
@@ -0,0 +1,19 @@
+import { GetRecipes } from "./supporting-files/data-access";
+import { ExpectedRecipeSummary, RunTest } from "./supporting-files/testing";
+
+console.clear();
+console.log("Expected Result Is:", ExpectedRecipeSummary);
+
+const recipeData = GetRecipes(); // the list of 1 recipe you should calculate the information for
+console.log("Recipe Data:", recipeData);
+const recipeSummary: any = {}; // the final result to pass into the test function
+/*
+ * YOUR CODE GOES BELOW THIS, DO NOT MODIFY ABOVE
+ * (You can add more imports if needed)
+ * */
+
+/*
+ * YOUR CODE ABOVE THIS, DO NOT MODIFY BELOW
+ * */
+RunTest(recipeSummary);
+
